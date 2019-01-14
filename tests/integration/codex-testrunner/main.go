568ad2238da1f12bf1886d56b14bd7bc292f268e
Law <ivan_law@cable.comcast.com>
2019-01-14 15:44:45 -0800

    updated license

--- tests/integration/codex-testrunner/main.go
+++ tests/integration/codex-testrunner/main.go
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2019 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package main
 
 import (
@@ -13,7 +30,6 @@ func main() {
 	locationoffeaturefiles := flag.String("feature", "", "path to feature files")
 	tags := flag.String("tags", "", "tags of testcases to be run")
 	flag.Parse()
-	//tags := "@HelloWorld001"
 	status := godog.RunWithOptions("godogs", func(s *godog.Suite) {
 		FeatureContext(s)
 	}, godog.Options{
