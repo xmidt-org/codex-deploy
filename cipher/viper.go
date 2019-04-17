9f175bd471114f781b097b4784804253e1e5131e
Jack Murdock <kcajmagic@gmail.com>
2019-04-17 13:51:19 -0700

    adding basic logger

--- cipher/viper.go
+++ cipher/viper.go
@@ -2,6 +2,8 @@ package cipher
 
 import (
 	"encoding/json"
+	"github.com/Comcast/webpa-common/logging"
+	"github.com/go-kit/kit/log"
 	"github.com/goph/emperror"
 	"github.com/spf13/viper"
 )
@@ -17,16 +19,26 @@ const (
 	CipherKey = "cipher"
 )
 
-type Options []Config
+type Options struct {
+	Logger log.Logger
+
+	o []Config
+}
 
 type Ciphers struct {
+	Logger log.Logger
+
 	options map[string]Decrypt
 }
 
 func (o Options) LoadEncrypt() (Encrypt, error) {
+	if o.Logger == nil {
+		o.Logger = logging.DefaultLogger()
+	}
 	var lastErr error
-	for _, elem := range o {
+	for _, elem := range o.o {
 		if encrypter, err := elem.LoadEncrypt(); err == nil {
+			elem.Logger = o.Logger
 			return encrypter, nil
 		} else {
 			lastErr = err
@@ -39,7 +51,11 @@ func PopulateCiphers(o Options) Ciphers {
 	c := Ciphers{
 		options: map[string]Decrypt{},
 	}
-	for _, elem := range o {
+	if o.Logger == nil {
+		o.Logger = logging.DefaultLogger()
+	}
+	for _, elem := range o.o {
+		elem.Logger = o.Logger
 		if decrypter, err := elem.LoadDecrypt(); err == nil {
 			c.options[elem.KID] = decrypter
 		}
@@ -60,7 +76,7 @@ func FromViper(v *viper.Viper) (o Options, err error) {
 	obj := v.Get("cipher")
 	data, err := json.Marshal(obj)
 	if err != nil {
-		return []Config{}, emperror.Wrap(err, "failed to load cipher config")
+		return Options{o: []Config{}}, emperror.Wrap(err, "failed to load cipher config")
 	}
 
 	err = json.Unmarshal(data, &o)
