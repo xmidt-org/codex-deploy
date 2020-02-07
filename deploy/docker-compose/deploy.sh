f30ec9215786bcba758b5989142c65926c1bcca5
Jack Murdock <kcajmagic@gmail.com>
2020-02-06 16:09:55 -0800

    fix db with new schema (#108)

--- deploy/docker-compose/deploy.sh
+++ deploy/docker-compose/deploy.sh
@@ -4,6 +4,9 @@ DIR=$( cd $(dirname $0) ; pwd -P )
 ROOT_DIR=$DIR/../../
 
 echo "Running services..."
-GUNGNIR_VERSION=${GUNGNIR_VERSION:-0.10.1-rc.1} \
-SVALINN_VERSION=${SVALINN_VERSION:-0.12.0} \
+GUNGNIR_VERSION=${GUNGNIR_VERSION:-0.12.3} \
+SVALINN_VERSION=${SVALINN_VERSION:-0.14.0} \
 docker-compose -f $ROOT_DIR/deploy/docker-compose/docker-compose.yml up -d $@
+sleep 5
+
+docker exec -it yb-tserver-n1 /home/yugabyte/bin/cqlsh yb-tserver-n1 -f /create_db.cql
